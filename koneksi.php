fee41e802263d46313557dc027ed8d672e4f49f7
salsagemintang <46420684+salsagemintang@users.noreply.github.com>
2019-03-06 09:41:19 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+ $host = "localhost";//host database
+ $username = "root";//username database
+ $password = "";//password database
+$db = "profile";
+
+ $koneksi = new mysqli($host, $username, $password, $db); //membuat koneksi ke server mysql
+
+if ($koneksi->connect_error){
+    die ("Koneksi Ke Database Gagal");
+}
+?>
